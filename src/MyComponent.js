00863fda8bfabc2a793b157d3c05610688a1fbd0
dhkang <verdure0448@gmail.com>
2022-01-13 10:02:23 +0900

    3.3 props 기본

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const MyComponent = () => {
+const MyComponent = props => {
     return (
         <div>
-            첫번째 컴포넌트
+            안녕하세요, 제 이름은 {props.name}입니다. <br/>
+            children 값은 {props.children} 입니다.
         </div>
     );
 };
+MyComponent.defaultProps = {
+    name: '철수'
+}
 
 export default MyComponent;

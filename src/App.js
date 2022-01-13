f19d6df9c469b98b20862cb73bc21d65e5e56971
dhkang <verdure0448@gmail.com>
2022-01-13 09:44:15 +0900

    3.2 첫 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 // import logo from './logo.svg';
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
-
-class App extends Component {
-  render() {
-    const name = 'react';
-    return <div className='react'>{name}</div>;
-  }
+import MyComponent from './MyComponent'
+const App = () => {
+  return <MyComponent />;
 }
 
 
---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyComponent = () => {
+    return (
+        <div>
+            첫번째 컴포넌트
+        </div>
+    );
+};
+
+export default MyComponent;

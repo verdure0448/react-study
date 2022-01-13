946f6e4a080a05309f2ace6cc1d800c5e9fd017b
dhkang <verdure0448@gmail.com>
2022-01-13 13:16:38 +0900

    3.3.6 PropTypes 기본 사용법

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import MyComponent from './MyComponent'
 const App = () => {
-  return <MyComponent>React</MyComponent>;
+  return <MyComponent name="React" favoriteNum={3}>React</MyComponent>;
 }
 
 

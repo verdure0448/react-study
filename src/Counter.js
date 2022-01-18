fe0df53bb1e966abf8224e298aad07902bd3be96
dhkang <verdure0448@gmail.com>
2022-01-18 16:37:48 +0900

    3.4.1.2 stateㄹ를 constructor에서 꺼내기

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
             this.setState({ number: number + 1 });

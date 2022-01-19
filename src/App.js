b985b5162f6a1481fc51316bbab15f90498472aa
dhkang <verdure0448@gmail.com>
2022-01-19 11:23:27 +0900

    4.2.2.1 onChange 이벤트 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 import React from "react";
 import "./App.css";
-import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 const App = () => {
-  return <Say />;
+  return <EventPractice />;
 };
 
 export default App;

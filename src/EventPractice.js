291b73ddf193765e3c9377c40a828c9f4f989a7f
dhkang <verdure0448@gmail.com>
2022-01-19 11:50:56 +0900

    4.2.3.2 property를 사용한 이벤트 바인딩

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -4,6 +4,28 @@ class EventPractice extends Component {
   state = {
     message: "",
   };
+
+  //   constructor(prop) {
+  //     super(prop);
+  //     this.handleChange = this.handleChange.bind(this);
+  //     this.handleClick = this.handleClick.bind(this);
+  //   }
+
+  // method, property 선언 방법에 따라 생성자 사용 유무가 결정됨
+  // method 는 생성자 선언과 바인딩이 필요
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  };
+
   render() {
     return (
       <div>
@@ -13,22 +35,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          onChange={this.handleChange}
         ></input>
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: "",
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
